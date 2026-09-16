9c42bb6476921a8e0c4cd8d0b01a0c74a5d4b355
YUVARAJ-VASI <yve4325@gmail.com>
2026-09-16 21:28:23 +0530

    Added RouteConfig route (request respective service)

--- Services/apiGateWay/src/main/java/com/task/apiGateWay/config/RouteConfig.java
+++ Services/apiGateWay/src/main/java/com/task/apiGateWay/config/RouteConfig.java
@@ -4,22 +4,41 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 public class RouteConfig {
 
     @Bean
-    RouteLocator routeLocator(RouteLocatorBuilder locatorBuilder){
+    RouteLocator routeLocator(RouteLocatorBuilder locatorBuilder) {
 
         return locatorBuilder.routes()
 
-                .route("employee-route",
-                        emp -> emp.path("/employee/**")
-                        .uri("lb://employee-service"))
+                .route("save-employee-route",
+                        emp -> emp
+                                .path("/employee/saveEmp")
+                                .and()
+                                .method(HttpMethod.POST)
+                                .and()
+                                .method(HttpMethod.PATCH,HttpMethod.GET,HttpMethod.DELETE)
+                                .negate()
+                                .uri("lb://employee-service")
+                )
+
+                .route("get-employee-route",
+                        emp -> emp
+                                .path("/employee/getAllEmp")
+                                .or()
+                                .path("/employee/getEmp/{id:[0-9]+}")
+                                .and()
+                                .method(HttpMethod.GET)
+                                .uri("lb://employee-service")
+                )
 
                 .route("department-route",
                         dep -> dep.path("/department/**")
-                        .uri("lb://department-service"))
+                                .uri("lb://department-service")
+                )
 
                 .build();
     }
